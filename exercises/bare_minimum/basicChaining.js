ae9c08771d69b98accfa49233270461cee16c613
aphavichitr <andrew.phavichitr@gmail.com>
2016-05-12 15:43:22 -0700

    Implement fetchProfileAndWriteToFile

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -9,13 +9,55 @@
  */
 
 var fs = require('fs');
-var Promise = require('bluebird');
+var promisification = require('./promisification');
+var promiseConstructor = require('./promiseConstructor');
 
+var Promise = require('bluebird');
+Promise.promisifyAll(fs);
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  return promiseConstructor.pluckFirstLineFromFileAsync(readFilePath)
+    .then(function(username) {
+      return promisification.getGitHubProfileAsync(username);
+    })
+    .then(function(responseBody) {
+      console.log(JSON.stringify(responseBody));
+      return fs.writeFileAsync(writeFilePath, JSON.stringify(responseBody));
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
 };
+
+// var request = require('request');
+// Promise.promisifyAll(request, {multiArgs: true});
+
+// // var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
+// //   return fs.readFileAsync(readFilePath, 'utf8')
+//     .then(function(content) {
+//       var firstLine = content.slice(0, content.indexOf('\n'));
+//       return firstLine;
+//     })
+//     .then(function(username) {
+//       var options = {
+//         url: 'https://api.github.com/users/' + username,
+//         headers: { 'User-Agent': 'request' },
+//         json: true  // will JSON.parse(body) for us
+//       };
+
+//       return request.getAsync(options);
+//     })
+//     .then(function(result) {
+//       // response
+//       // var response = result[0];
+//       var body = result[1];
+//       return fs.writeFileAsync(writeFilePath, JSON.stringify(body));
+//     })
+//     .catch(function(err) {
+//       console.log('Error: ' + err);
+//     });
+// };
 
 // Export these functions so we can test them
 module.exports = {
